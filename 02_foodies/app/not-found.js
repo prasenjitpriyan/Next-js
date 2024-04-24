0bc4564489e9046f53eb094f76f4c2710f77ec6d
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-24 20:57:21 +0530

    not found page added

---
+++ 02_foodies/app/not-found.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>
+        Unfortunately, we counld not find the requested page or resource. Please
+        try again later
+      </p>
+    </main>
+  );
+}
