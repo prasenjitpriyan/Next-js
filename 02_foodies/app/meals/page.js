c7265a448598bc4c084ca515ed20ea95e9d622e5
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-24 20:52:38 +0530

    error added

--- 02_foodies/app/meals/page.js
+++ 02_foodies/app/meals/page.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+const Meals = async () => {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
 const MealsPage = () => {
   return (
@@ -19,7 +25,11 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </React.Fragment>
   );
