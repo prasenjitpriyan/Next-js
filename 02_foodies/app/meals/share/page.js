05cdef104f9876f83d9dff98328d676e43fbe550
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-25 20:13:07 +0530

    add new meal

--- 02_foodies/app/meals/share/page.js
+++ 02_foodies/app/meals/share/page.js
@@ -1,9 +1,15 @@
+"use client";
+
 import React from "react";
+import { useFormState } from "react-dom";
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 const ShareMealPage = () => {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <React.Fragment>
       <header className={classes.header}>
@@ -13,7 +19,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,8 +48,9 @@ const ShareMealPage = () => {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
