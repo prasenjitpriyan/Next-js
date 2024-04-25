e8b7126f63d3c13940206163b64ab16359330f6b
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-25 20:36:05 +0530

    dynamic metadata

--- 02_foodies/app/meals/[mealSlug]/page.js
+++ 02_foodies/app/meals/[mealSlug]/page.js
@@ -4,6 +4,17 @@ import Image from "next/image";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export async function generatedMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+   if (!meal) {
+     notFound();
+   }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 const MealDetailsPage = ({ params }) => {
   const meal = getMeal(params.mealSlug);
 
